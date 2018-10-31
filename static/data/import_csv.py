756c38e69e0ab76b1028cd29d4a775a8235ec874
Kristen Rutherford <kmr4@rice.edu>
2018-10-31 16:20:30 -0500

    trying heroku

--- static/data/import_csv.py
+++ static/data/import_csv.py
@@ -2,8 +2,12 @@
 
 import psycopg2
 import pandas as pd
+import os
 
-conn = psycopg2.connect("host=localhost dbname=midterm_db user=postgres password=Winchester110283")
+#conn = psycopg2.connect("host=localhost dbname=midterm_db user=postgres password=Winchester110283")
+#DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = "postgres://spajdxucccmxnr:c02e9213c4168046fb6c5097341661c4d28dd265a9d6830f5950b0e46ad10621@ec2-23-23-153-145.compute-1.amazonaws.com:5432/ddlqkveshrq9mv"
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
 df_condos = pd.read_csv('midterm.csv', encoding='ISO-8859-1')
